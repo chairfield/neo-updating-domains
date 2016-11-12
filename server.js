2c54bbd41af16b7a7df6ce8f0ad785fef1298b97
Chris Hairfield <chairfield@gmail.com>
2016-11-12 12:53:51 -0700

    installed swagger UI

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var Http = require('http');
 var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
+var SwaggerUi = require('swaggerize-ui');
 var Path = require('path');
 
 var App = Express();
@@ -17,11 +18,15 @@ App.use(BodyParser.urlencoded({
 
 App.use(Swaggerize({
     api: Path.resolve('./config/swagger.json'),
-    docspath: '/',
+    docspath: '/docs',
     handlers: Path.resolve('./handlers')
 }));
 
-Server.listen(8000, function () {
+App.use('/', SwaggerUi({
+    docs: '/docs'
+}));
+
+Server.listen(8000, '127.0.0.1', function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
     console.log('App running on %s', App.swagger.api.host);
