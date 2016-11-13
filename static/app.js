f9ee3f4e50fd45ad0a0b1c0a528d816a0b98f819
Chris Hairfield <chairfield@gmail.com>
2016-11-13 00:09:13 -0700

    may specify domain and graph updates

--- static/app.js
+++ static/app.js
@@ -1,30 +1,12 @@
 
 $(function() {
-    //search();
+    var width = $(window).width();
+    var height = $(window).height() - 100;
+    var force = d3.layout.force().charge(-10).linkDistance(100).size([width, height]);
 
-    /*$("#search").submit(e => {
-        e.preventDefault();
-    search();*/
-
-    window.client = new SwaggerClient({
-        url: "/docs",
-        success: function() {
-            client.query.queryByDomain(
-                { domain: "google.com", depth: 1 },
-                { responseContentType: 'application/json' },
-                function(res) {
-                    renderGraph(res.obj);
-                });
-        }
-    });
-});
-
-function renderGraph(graph) {
-    var width = 800, height = 800;
-    var force = d3.layout.force().charge(-200).linkDistance(30).size([width, height]);
-
-    var svg = d3.select("#graph").append("svg")
-        .attr("width", "100%").attr("height", "100%")
+    var container = d3.select("#graph");
+    var svg = container.append("svg")
+        .attr("width", "100%").attr("height", "90%")
         .attr("pointer-events", "all");
 
     // Background
@@ -33,21 +15,53 @@ function renderGraph(graph) {
         .attr("height", "100%")
         .attr("fill", "pink");
 
-    force.nodes(graph.nodes).links(graph.links).start();
+    window.client = new SwaggerClient({
+        url: "/docs",
+        success: function() {
+            $("#queryBtn").click(function() {
+                var domain = $("#domainInput").val();
+                client.query.queryByDomain(
+                    { domain: domain, depth: 1 },
+                    { responseContentType: 'application/json' },
+                    function(res) {
+                        updateGraph(res.obj);
+                    });
+            });
+        }
+    });
 
-    var link = svg.selectAll(".link")
-        .data(graph.links).enter()
-        .append("line").attr("class", "link");
+    function updateGraph(data) {
+        if (data.nodes.length > 0) {
+            // Fix the root node in the middle
+            data.nodes[0].fixed = true;
+            data.nodes[0].x = width / 2;
+            data.nodes[0].y = height / 2;
+        }
 
-    var node = svg.selectAll(".node")
-        .data(graph.nodes).enter()
-        .append("circle")
-        .attr("class", "node")
-        .attr("r", 10)
-        .call(force.drag);
+        force.nodes(data.nodes).links(data.links).start();
 
-    force.on("tick", function() {
-        link.attr("x1", function(d) {
+        svg.selectAll(".link")
+            .data([])
+            .exit().remove();
+
+        svg.selectAll(".node")
+            .data([])
+            .exit().remove();
+
+        var link = svg.selectAll(".link")
+            .data(data.links)
+            .enter().append("line")
+            .attr("class", "link");
+
+        var node = svg.selectAll(".node")
+            .data(data.nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", 10)
+            .call(force.drag);
+
+        force.on("tick", function() {
+            link.attr("x1", function(d) {
                 return d.source.x;
             }).attr("y1", function(d) {
                 return d.source.y;
@@ -57,10 +71,11 @@ function renderGraph(graph) {
                 return d.target.y;
             });
 
-        node.attr("cx", function(d) {
+            node.attr("cx", function(d) {
                 return d.x;
             }).attr("cy", function(d) {
                 return d.y;
             });
         });
-}
+    }
+});
